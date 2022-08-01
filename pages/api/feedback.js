b74335eb6707c66aeab632719f1e37a26a6aca07
Pedro Tigre <pedrogtigre@gmail.com>
2022-07-31 21:49:11 -0300

    getFeedback function added, and code was organized

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,19 +1,40 @@
 import fs from 'fs/promises';
 import path from 'path';
 
+function getFeedbackPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+}
+async function getFileData(path) {
+  return JSON.parse(await fs.readFile(path));
+}
+
+async function setFeedbackData(path, newData) {
+  return fs.writeFile(path, JSON.stringify(newData));
+}
+
 async function handler(req, res) {
+  // New feedback structure
+  const newFeedback = {
+    id: new Date().toISOString(),
+    email: req.body.email,
+    text: req.body.text,
+  };
+
+  // Get feedback path
+  const filePath = getFeedbackPath();
+  // Get the data stored on feedback.json (an array)
+  const data = await getFileData(filePath);
+
   if (req.method === 'POST') {
-    const newFeedback = {
-      id: new Date().toISOString(),
-      email: req.body.email,
-      text: req.body.text,
-    };
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    const fileData = JSON.parse(await fs.readFile(filePath));
-    fileData.push(newFeedback);
-    const fileWrite = await fs.writeFile(filePath, JSON.stringify(fileData));
+    //  Add new feedback by pushing it to the data array.
+    data.push(newFeedback);
+    // Update feedback.json with the new data
+    await setFeedbackData(filePath, data);
+
     return res.status(201).json({ message: 'Feedback received!' });
   }
-  return res.status(200).json({ message: 'Success!' });
+  if (req.method === 'GET') {
+    return res.status(200).json({ feedback: data });
+  }
 }
 export default handler;
